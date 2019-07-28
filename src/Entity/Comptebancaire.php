cd36782831697c2582b5805118c4986e214a95f9
Fatou Diouf <diouff89@gmail.com>
2019-07-28 17:09:01 +0000

    git init

---
+++ src/Entity/Comptebancaire.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ComptebancaireRepository")
+ */
+class Comptebancaire
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numerocompte;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $montant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumerocompte(): ?int
+    {
+        return $this->numerocompte;
+    }
+
+    public function setNumerocompte(int $numerocompte): self
+    {
+        $this->numerocompte = $numerocompte;
+
+        return $this;
+    }
+
+    public function getMontant(): ?int
+    {
+        return $this->montant;
+    }
+
+    public function setMontant(int $montant): self
+    {
+        $this->montant = $montant;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Partenaire.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PartenaireRepository")
+ */
+class Partenaire
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ninea;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $raisonsociale;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="partenaire")
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNinea(): ?string
+    {
+        return $this->ninea;
+    }
+
+    public function setNinea(string $ninea): self
+    {
+        $this->ninea = $ninea;
+
+        return $this;
+    }
+
+    public function getRaisonsociale(): ?string
+    {
+        return $this->raisonsociale;
+    }
+
+    public function setRaisonsociale(string $raisonsociale): self
+    {
+        $this->raisonsociale = $raisonsociale;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
